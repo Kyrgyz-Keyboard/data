547114180a97027546f7df680d27c8384348a964
Nikita Paniukhin <git@npanuhin.me>
2025-03-17 21:31:32 +0100

    Update

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,11 @@
 import re
+import os
+
+
+FILE_LOCATION = os.path.dirname(os.path.abspath(__file__))
+
+
+# ======================================================== REGEX ========================================================
 
 REPLACEMENTS = {
     'c': 'с',
@@ -21,79 +28,25 @@ WORD_PATTERN = re.compile(r'\b[А-Яа-яcopaeyёңөү]+(?:[-–—][А-Яа-я
 
 INNER_CLEAN_PATTERN = re.compile(r'[-–—]')
 
-# ======================================================================================================================
+# ===================================================== DICTIONARY =====================================================
 
+print('Loading dictionary...')
+DICTIONARY = []
+with open(f'{FILE_LOCATION}/../results/all_words_from_dictionary_by_base.txt', 'r', encoding='utf-8') as file:
+    for line in filter(None, map(str.strip, file)):
+        if not line.startswith('├╴'):
+            DICTIONARY.append((line, []))
+        else:
+            DICTIONARY[-1][1].append(line.removeprefix('├╴'))
 
-class SuffixTrie:
-    def __init__(self, suffixes: set[str]):
-        self.trie = {}
-        for suffix in suffixes:
-            node = self.trie
-            for char in reversed(suffix):
-                node = node.setdefault(char, {})
-            node['$'] = suffix
+print(f'[Dictionary] Base words: {len(DICTIONARY)}')
 
-    def remove_suffix(self, word: str) -> str:
-        node = self.trie
-        for i in range(len(word) - 1, -1, -1):
-            char = word[i]
-            if char not in node:
-                break
-            node = node[char]
-            if '$' in node:
-                return word[:i]
-        return word
+WORD_TO_BASE = {}
+for word, forms in DICTIONARY:
+    WORD_TO_BASE[word] = word
+    for form in forms:
+        # if form in WORD_TO_BASE:
+        #     print(form)
+        WORD_TO_BASE[form] = word
 
-
-SUFFIXES = {
-    'чы', 'чи', 'чу', 'чү',
-    'ба', 'бе', 'бо', 'бө',
-    'мын', 'мин', 'мун', 'мүн',
-    'сың', 'сиң', 'суң', 'сүң',
-    'сыз', 'сиз', 'суз', 'сүз',
-    'быз', 'биз', 'буз', 'бүз',
-    'пыз', 'пиз', 'пуз', 'пүз',
-    'сыңар', 'сиңер', 'суңар', 'сүңөр',
-    'сыздар', 'сиздер', 'суздар', 'сүздөр',
-    'нын', 'нин', 'нун', 'нүн',
-    'дын', 'дин', 'дун', 'дүн',
-    'тын', 'тин', 'тун', 'түн',
-    'га', 'ге', 'го', 'гө',
-    'ка', 'ке', 'ко', 'кө',
-    'дар', 'дер', 'дор', 'дөр',
-    'тар', 'тер', 'тор', 'төр',
-    'лар', 'лер', 'лор', 'лөр',
-
-    'па', 'пе', 'по', 'пө',
-    'би', 'бу', 'бү', 'бы',
-    'пи', 'пу', 'пү', 'пы',
-
-    'тон', 'төн', 'тан', 'тен',
-    'дон', 'дөн', 'дан', 'ден',
-
-    'ги', 'гу', 'гы', 'гү',
-    'ки', 'ку', 'кы', 'кү',
-    'ка', 'ке', 'ко', 'кө',
-
-    'гун', 'гүн', 'гын', 'гин',
-    'кун', 'күн', 'кын', 'кин',
-
-    'даш', 'деш', 'дош', 'дөш',
-    'таш', 'теш', 'тош', 'төш',
-
-    'йм', 'өм', 'ам', 'ем', 'ом',
-
-    'да', 'де', 'до', 'дө',
-    'та', 'те', 'то', 'тө',
-    'ына',
-
-    'лик', 'лак', 'лөк', 'лок', 'лук', 'лүк',
-
-    'ум', 'үм', 'өм'
-}
-SUFFIXES = {suffix.translate(TRANSLATION_TABLE) for suffix in SUFFIXES}
-suffix_trie = SuffixTrie(SUFFIXES)
-
-
-def remove_suffixes(word: str) -> str:
-    return suffix_trie.remove_suffix(word)
+print(f'[Dictionary] Word forms: {len(WORD_TO_BASE)}')
