f4596c2c0794638b5749fcdfe94b6c807b4897d6
Nikita Paniukhin <git@npanuhin.me>
2025-03-22 12:53:27 +0100

    Minor refactoring

--- dictionary/kyrgyz_tili.py
+++ dictionary/kyrgyz_tili.py
@@ -14,12 +14,20 @@ import kg.db.generate_words as kyrgyz_tili_generate
 def gen():
     print('Generating dictionary from kyrgyz_tili...')
 
+    bases_count = forms_count = 0
+
     with open(mkpath(ROOT, 'results/kyrgyz_tili_words_by_base.txt'), 'w', encoding='utf-8') as file:
         os.chdir('kyrgyz_tili')
         for word in kyrgyz_tili_generate.Word.select():
+            forms = kyrgyz_tili_generate.generate_all_children(word)
+            bases_count += 1
+            forms_count += len(forms)
             file.write(word.word + ''.join(
-                f'\n├╴{form}' for form in islice(kyrgyz_tili_generate.generate_all_children(word), 1, None)
+                f'\n├╴{form}' for form in islice(forms, 1, None)
             ) + '\n\n')
+
+    print(f'Total word bases: {bases_count}')
+    print(f'Total word forms: {forms_count}')
 
 
 if __name__ == '__main__':
