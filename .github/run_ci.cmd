390b4b3b0a72e391dd0df5ec6a44eddc0394e408
Nikita Paniukhin <git@npanuhin.me>
2025-04-18 20:05:57 +0200

    Updated tokenizer and added tests for it

--- .github/run_ci.cmd
+++ .github/run_ci.cmd
@@ -1,1 +1,1 @@
-cls && flake8 && mypy . --config-file tox.ini --install-types --non-interactive && pytest
+cls && flake8 && mypy . --config-file tox.ini --install-types --non-interactive && py -3 -m pytest -vv
