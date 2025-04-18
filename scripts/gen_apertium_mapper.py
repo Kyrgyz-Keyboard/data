0957364eae74bdec2f6f4933858e448a2a97ff9a
Nikita Paniukhin <git@npanuhin.me>
2025-04-19 00:40:02 +0200

    Small Apertium mapper improvements

--- scripts/gen_apertium_mapper.py
+++ scripts/gen_apertium_mapper.py
@@ -27,20 +27,24 @@ def chunkify(array: list[T], n: int) -> Iterator[list[T]]:
     )
 
 
-def process_chunk(words_chunk: list[str], chunk_num: int, apertium_mapper: dict[str, str]) -> list[list[str]]:
-    analyzer = apertium.Analyzer('kir')
+def process_chunk(
+    words_chunk: list[str],
+    chunk_num: int,
+    apertium_mapper: dict[str, str | None]
+) -> list[tuple[str, str | None]]:
 
+    analyzer = apertium.Analyzer('kir')
     results = []
 
     for i, word in enumerate(words_chunk, 1):
-        if word in apertium_mapper:
-            continue
+        if word not in apertium_mapper:
+            bases = []
+            for reading in analyzer.analyze(word)[0].readings:
+                cur_base = reading[0].baseform.replace(' ', '')
+                if cur_base[0] != '*':
+                    bases.append(cur_base)
 
-        base = min(
-            reading[0].baseform.lstrip('*')
-            for reading in analyzer.analyze(word)[0].readings
-        )
-        results.append([word, base])
+            results.append((word, min(bases) if bases else None))
 
         if i % 1000 == 0:
             print_async(f'Processed {i}/{len(words_chunk)} words in chunk {chunk_num}')
@@ -59,35 +63,14 @@ def create_apertium_mapper():
         }
 
     with open(mkpath('../results/apertium_mapper.txt'), 'r', encoding='utf-8') as file:
-        apertium_mapper: dict[str, str] = dict(
-            list(map(str.strip, line.split(' ', 1)))
-            for line in filter(None, file)
-        )
+        apertium_mapper: dict[str, str | None] = {}
 
-    to_process = list(set(words_indexed.keys()) - set(apertium_mapper.keys()))
-
-    num_workers = 4
-    chunks = chunkify(to_process, num_workers)
-
-    print(f'Processing {len(to_process)} words in {num_workers} parallel chunks...')
-
-    results: list[list[str]] = []
-
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        for i, future in enumerate(as_completed(
-            executor.submit(process_chunk, chunk, chunk_num, apertium_mapper)
-            for chunk_num, chunk in enumerate(chunks, 1)
-        ), 1):
-            chunk_result = future.result()
-            results.extend(chunk_result)
-            print(f'Processed chunk {i} ({len(chunk_result)} words)')
-
-    results.sort(key=lambda x: words_indexed[x[0]])
-
-    for word, base in results:
-        apertium_mapper[word] = base
-
-    print(f'Added {len(results)} new words to the mapper')
+        for line in map(str.strip, filter(None, file)):
+            if ' ' in line:
+                word, base = map(str.strip, line.split(' '))
+                apertium_mapper[word] = base
+            else:
+                apertium_mapper[line.strip()] = None
 
     old_size = len(apertium_mapper)
 
@@ -97,13 +80,38 @@ def create_apertium_mapper():
 
     print(f'Removed {old_size - len(apertium_mapper)} words from the mapper')
 
+    to_process = list(set(words_indexed.keys()) - set(apertium_mapper.keys()))
+
+    num_workers = 4
+    chunks = chunkify(to_process, num_workers)
+
+    print(f'Processing {len(to_process)} words in {num_workers} parallel chunks...')
+
+    added_count = 0
+
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        for i, future in enumerate(as_completed(
+            executor.submit(process_chunk, chunk, chunk_num, apertium_mapper)
+            for chunk_num, chunk in enumerate(chunks, 1)
+        ), 1):
+            chunk_result = future.result()
+            for word, base_opt in chunk_result:
+                apertium_mapper[word] = base_opt
+            added_count += len(chunk_result)
+            print(f'Processed chunk {i} ({len(chunk_result)} words)')
+
+    print(f'Added {added_count} new words to the mapper')
+
     assert len(apertium_mapper) == len(words_indexed)
 
     apertium_mapper['жана'] = 'жана'
 
     write_file(
         mkpath('../results/apertium_mapper.txt'),
-        '\n'.join(f'{word} {base}' for word, base in apertium_mapper.items())
+        '\n'.join(
+            (word if base is None else f'{word} {base}')
+            for word, base in sorted(apertium_mapper.items(), key=lambda x: words_indexed[x[0]])
+        )
     )
 
 
