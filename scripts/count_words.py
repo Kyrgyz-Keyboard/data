7fe2805ec1e4d05f1d36aff7af7bb162af22197b
Nikita Paniukhin <git@npanuhin.me>
2025-05-05 01:59:44 +0200

    Updated tokenizer

--- scripts/count_words.py
+++ scripts/count_words.py
@@ -8,16 +8,33 @@ mkpath = PathMagic(__file__)
 
 
 def count_words():
-    with open(mkpath('../results/word_freq.txt'), 'r', encoding='utf-8') as file:
-        print('Words:\t\t\t\t', sum(1 for _ in filter(None, file)))
+    letters: set[str] = set()
 
-    with open(mkpath('../results/base_simple_freq.txt'), 'r', encoding='utf-8') as file:
-        print('Base simple words:\t', sum(1 for _ in filter(None, file)))
+    with open(mkpath('../results/word_freq.txt'), 'r', encoding='utf-8') as file:
+        for word_count, line in enumerate(filter(None, file)):
+            word, _ = line.split(' ')
+            letters.update(word)
+            word_count += 1
+
+        print('Words:\t\t\t', word_count)
+
+    with open(mkpath('../results/base_apertium_freq.txt'), 'r', encoding='utf-8') as file:
+        print('Apertium words:\t', sum(1 for _ in filter(None, file)))
 
     with open(mkpath('../results/apertium_mapper.txt'), 'r', encoding='utf-8') as file:
-        apertium_mapper = [list(map(str.strip, line.split(' ', 1))) for line in filter(None, file)]
-        apertium_mapper_reverse = {apertium_base for _, apertium_base in apertium_mapper}
-        print(len(apertium_mapper), 'words mapped to', len(apertium_mapper_reverse), 'apertium base words')
+        apertium_mapper_keys, apertium_mapper_values = set(), set()
+        for line in filter(None, file):
+            if ' ' in line:
+                key, value = line.split(' ')
+                apertium_mapper_keys.add(key)
+                apertium_mapper_values.add(value)
+            else:
+                apertium_mapper_keys.add(line)
+        print(len(apertium_mapper_keys), 'words mapped to', len(apertium_mapper_values), 'apertium base words')
+
+    print()
+    print('Distinct letters:', len(letters))
+    print(''.join(sorted(letters)))
 
 
 if __name__ == '__main__':
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,5 +1,7 @@
+from typing import Generator
+
+import regex
 import sys
-import re
 
 if __name__ == '__main__':
     sys.path.append('../')
@@ -27,26 +29,35 @@ class Tokenizer:
         # '-': None,
         # '–': None,
         # '—': None,
+
+        'ї': 'ү',
+        'є': 'ө',
+        'ў': 'ң',
     }
     REPLACEMENTS.update({k.upper(): (v.upper() if v else v) for k, v in REPLACEMENTS.items()})
     TRANSLATION_TABLE = str.maketrans(REPLACEMENTS)
 
-    # WORD_PATTERN = re.compile(r'\b[А-Яа-я]+(?:[-–—][А-Яа-я]+)*\b')
-    WORD_PATTERN = re.compile(
+    # WORD_PATTERN = regex.compile(r'\b[А-Яа-я]+(?:[-–—][А-Яа-я]+)*\b')
+    WORD_PATTERN = regex.compile(
         r"""
-        (?:
-        # Main pattern
-          [а-яa-zёңөүА-ЯA-ZЁҢҮӨ](?:[а-яa-zёңөүА-ЯA-ZЁҢҮӨ\.\:]*[а-яa-zёңөүА-ЯA-ZЁҢҮӨ])?
-        # Pure numbers + (23:59) + (1.5 / 1,5)
-        | [0-9](?:[0-9\:\.\,]*[0-9])?
-        ) (?=\b|\d)
+            (?:
+            # Main pattern: letters (1 letter does not count!)
+            (?<=\b|[0-9])
+            [а-яa-zёңөүА-ЯA-ZЁҢҮӨ][а-яa-zёңөүА-ЯA-ZЁҢҮӨ\.]*[а-яa-zёңөүА-ЯA-ZЁҢҮӨ]
+            (?=\b|[0-9])
+        ) | (?:
+            (?<=\b|[^0-9])
+            # Pure numbers + (23:59) + (1.5 / 1,5)
+            [0-9](?:[0-9\:\.\,]*[0-9])?
+            (?=\b|[^0-9])
+        )
         """,
-        flags=re.VERBOSE
+        flags=regex.VERBOSE
     )
 
-    # INNER_CLEAN_PATTERN = re.compile(r'[-–—]')
+    # INNER_CLEAN_PATTERN = regex.compile(r'[-–—]')
 
-    SENTENCE_SPLIT_PATERN = re.compile(
+    SENTENCE_SPLIT_PATERN = regex.compile(
         r"""
         # Linebreak is always a separator
           \n
@@ -63,18 +74,15 @@ class Tokenizer:
         # Dot after digit that follows with any letter
         | (?<=\d)\.\s*(?=[a-zа-яёңүөA-ZА-ЯЁҢҮӨ]|[^\w\s])
         """,
-        flags=re.VERBOSE
+        flags=regex.VERBOSE
     )
 
-    def __init__(self):
-        pass
-
-    def process_text(self, text: str):
+    def process_text(self, text: str) -> Generator[list[str]]:
         for sentence in filter(None, map(str.strip, Tokenizer.SENTENCE_SPLIT_PATERN.split(
             text.translate(Tokenizer.TRANSLATION_TABLE)
         ))):
             words = []
-            for word in map(re.Match.group, Tokenizer.WORD_PATTERN.finditer(sentence)):
+            for word in map(regex.Match.group, Tokenizer.WORD_PATTERN.finditer(sentence)):
                 if len(word) > 1 or word.isdigit():
                     words.append(word)
             if words:
@@ -88,13 +96,13 @@ if __name__ == '__main__':
     # print(tokenizer.WORD_PATTERN.pattern)
 
     # for filename in os.listdir(f'{file_location}/../results/texts'):
-    for filename in ('test.txt',):
+    for filename in ('test2.txt',):
         with open(mkpath(f'../results/{filename}'), 'r', encoding='utf-8') as file:
             text = file.read()
             # print(text)
 
             for sentence in tokenizer.process_text(text):
-                # print(sentence)
+                print(sentence)
                 for word in sentence:
                     assert ' ' not in word, filename
 
