c37ba2fbe4b93668c9d78f8432361f891f700423
Nikita Paniukhin <git@npanuhin.me>
2025-05-05 19:41:29 +0200

    Word predictor first attempt

---
+++ prediction/build_word_predictor.py
@@ -0,0 +1,90 @@
+from collections import deque, defaultdict, Counter
+from time import perf_counter
+import math
+import sys
+import os
+
+if __name__ == '__main__':
+    sys.path.append('../')
+
+from src.utils import PathMagic
+mkpath = PathMagic(__file__)
+
+from prediction.trie import dump_file as dump_trie_file
+
+
+LAYERS = [None, 30, 15]
+
+LOG_EVERY_N_BYTES = 30 * 1024 * 1024  # 30 MB
+# LOG_EVERY_N_BYTES = 1024  # 1 KB
+
+
+def size_to_str(size_bytes: int) -> str:
+    if size_bytes == 0:
+        return '0 B'
+    size_name = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return f'{s} {size_name[i]}'
+
+
+def build_trie():
+    print('Constructing words list...')
+    word_size = {}
+    word_freq = {}
+    with open(mkpath('../results/word_freq.txt'), 'r', encoding='utf-8') as file:
+        for line in map(str.strip, filter(None, file)):
+            word, freq = line.split()
+            word_freq[word] = int(freq)
+            word_size[word] = len(word.encode('utf-8'))
+
+    print('Building trie...')
+
+    file_size = os.path.getsize(mkpath('../results/sentences.txt'))
+    print(f'File size: {size_to_str(file_size)}')
+
+    layer1 = defaultdict(Counter)  # type: ignore
+
+    total_read_size = 0
+    next_log = LOG_EVERY_N_BYTES
+    start_time = perf_counter()
+
+    with open(mkpath('../results/sentences.txt'), 'r', encoding='utf-8') as file:
+        for sentence in map(str.strip, file):
+            word_window: deque[str] = deque(maxlen=1)
+
+            # if len(sentence.split(' ')) < 10:
+            #     continue
+
+            for word in sentence.split(' '):
+                # assert word_size[word], 'Empty word found'
+                total_read_size += word_size[word]
+
+                if word_freq[word] < 100:
+                    word_window.clear()
+                    continue
+
+                if word_window:
+                    layer1[word_window[-1]][word] += 1
+
+                word_window.append(word)
+
+            if total_read_size >= next_log:
+                print(
+                    f'Processed {size_to_str(total_read_size)} / {size_to_str(file_size)} bytes in '
+                    f'{perf_counter() - start_time:.0f} seconds'
+                )
+                next_log += LOG_EVERY_N_BYTES
+
+                break
+
+    for key, value in layer1.items():
+        layer1[key] = dict(sorted(value.items(), key=lambda x: x[1], reverse=True)[:LAYERS[1]])
+
+    dump_trie_file(layer1, mkpath('../results/trie.json'))
+    # print(layer1)
+
+
+if __name__ == '__main__':
+    trie = build_trie()
